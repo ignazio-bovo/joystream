313d88c84744ab22b607e64596cf0514eee5ac9c
ignazio-bovo <10487328@polimi.it>
2021-11-02 10:30:06 +0100

    changed scenario to content directory

--- tests/network-tests/run-test-scenario.sh
+++ tests/network-tests/run-test-scenario.sh
@@ -7,7 +7,7 @@ cd $SCRIPT_PATH
 # pass the scenario name without .ts extension
 SCENARIO=$1
 # fallback to full.ts scenario if not specified
-SCENARIO=${SCENARIO:=full}
+SCENARIO=${SCENARIO:=content-directory}
 
 # Execute the tests
 time DEBUG=integration-tests* yarn workspace network-tests node-ts-strict src/scenarios/${SCENARIO}.ts
--- tests/network-tests/src/scenarios/content-directory.ts
+++ tests/network-tests/src/scenarios/content-directory.ts
@@ -4,3 +4,4 @@ import { scenario } from '../Scenario'
 scenario(async ({ job }) => {
   job('setup content lead', leaderSetup.content)
 })
+
